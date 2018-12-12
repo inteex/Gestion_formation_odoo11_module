97dd8d0648c7516d68b2419fe55ef00a8cf173da
inteex13 <inteex13@gmail.com>
2018-12-12 09:43:36 +0100

    function to add all candidat

--- wizard/wizard_attestation.py
+++ wizard/wizard_attestation.py
@@ -8,5 +8,15 @@ class Wizard_attestation(models.TransientModel):
     date=fields.Date()
     description=fields.Text()
 
+    @api.multi
     def action_done(self):
-        print('cc')
+        session_id = self.env.context.get('active_id')
+        session = self.env['session'].browse(session_id)
+        for candidat in session.candidat_ids:
+            self.env['formation.attestation'].create({
+                'date' : self.date,
+                'description' : self.description,
+                'candidat' : candidat.id,
+                'formation' : session.formationId.id,
+            })
+
