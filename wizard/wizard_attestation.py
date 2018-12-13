da3cab230ac8a91b90caad5987aa95a7cf67536a
inteex13 <inteex13@gmail.com>
2018-12-13 21:28:52 +0100

    attestation and many2many field for candidat attestation

--- wizard/wizard_attestation.py
+++ wizard/wizard_attestation.py
@@ -5,18 +5,27 @@ from datetime import datetime
 class Wizard_attestation(models.TransientModel):
     _name="foramtion.wizard_attestation"
 
-    date=fields.Date()
-    description=fields.Text()
+    @api.multi
+    def candidats(self):
+        session_id = self.env.context.get('active_id')
+        session = self.env['session'].browse(session_id)
+        return  session.candidat_ids
 
     @api.multi
     def action_done(self):
         session_id = self.env.context.get('active_id')
         session = self.env['session'].browse(session_id)
-        for candidat in session.candidat_ids:
+        for candidat in self.candidat_ids:
             self.env['formation.attestation'].create({
-                'date' : self.date,
-                'description' : self.description,
-                'candidat' : candidat.id,
-                'formation' : session.formationId.id,
+                'date': self.date,
+                'description': self.description,
+                'candidat': candidat.id,
+                'formation': session.formationId.id,
             })
+
+
+    date=fields.Date()
+    description=fields.Text()
+    candidat_ids = fields.Many2many('formation.candidat','attestation_candidat','attestation_id','candidat_id',default=candidats)
+
 
